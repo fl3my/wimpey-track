179cc065e453466e14ab17a0bb194676f0429da8
fl3my <rossflem@icloud.com>
2026-01-21 14:36:23 +0000

    Add ability to create multiple books, depending on if the journey occurs between a claim rate change

--- backend/WimpeyTrack.Api/Controllers/ReportController.cs
+++ backend/WimpeyTrack.Api/Controllers/ReportController.cs
@@ -1,3 +1,4 @@
+using System.IO.Compression;
 using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,89 +22,160 @@ namespace WimpeyTrack.Api.Controllers
         public async Task<IActionResult> DownloadReport(DateOnly startDate, DateOnly endDate)
         {
             // Retrieve the data
-            var book = await CreateBookAsync(startDate, endDate);
+            var books = await CreateBooksAsync(startDate, endDate);
+
+            if (books.Count == 0)
+            {
+                return BadRequest();
+            }
             
             // Load template
             var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "template.xlsx");
-            using var workbook = new XLWorkbook(templatePath);
             
-            // Target the first mileage sheet
-            
-            // Fill Claim rate on first sheet
-            var expenseSheet = workbook.Worksheet(1);
-            expenseSheet.Cell(53, 2).Value = book.ClaimRate;
-            expenseSheet.Cell(49, 2).Value = "YES";
-            
-            // For each sheet
-            for(var sheetIndex = 0; sheetIndex < book.Sheets.Count; sheetIndex++)
+            // For testing purposes, put xlsx in zip archive
+            using var zipStream = new MemoryStream();
+
+            await using (var archive = new ZipArchive(zipStream, ZipArchiveMode.Create, true))
             {
-                // Get the sheet index
-                var sheet = book.Sheets.ElementAt(sheetIndex);
-                var worksheet = workbook.Worksheet(sheetIndex + 3); // Mileage sheet is the 2nd sheet
-                
-                // Get the starting row
-                int currentRow = 6;
-                
-                // Fill the rows downwards
-                foreach (var row in sheet.Rows)
+                // For each book
+                for (var bookIndex = 0; bookIndex < books.Count; bookIndex++)
                 {
-                    // Fill rows
-                    worksheet.Cell(currentRow, 1).Value = row.Date;
-                    worksheet.Cell(currentRow, 5).Value = row.TotalMiles;
+                    var book = books[bookIndex];
                     
-                    foreach (var description in row.Description)
+                    // Create an instance of ClosedXML
+                    using var workbook = new XLWorkbook(templatePath);
+
+                    // Fill Claim rate on first sheet
+                    var expenseSheet = workbook.Worksheet(1);
+                    expenseSheet.Cell(53, 2).Value = book.IsOverThreshold ? 0.25 : 0.45;
+                    expenseSheet.Cell(49, 2).Value = book.IsOverThreshold ? "YES" : "NO";
+
+                    // Fill sheets
+                    for (var sheetIndex = 0; sheetIndex < book.Sheets.Count; sheetIndex++)
                     {
-                        worksheet.Cell(currentRow, 2).Value = description;
-                        currentRow++;
+                        // Get the mileage detail sheet
+                        var sheet = book.Sheets.ElementAt(sheetIndex);
+                        var worksheet = workbook.Worksheet(sheetIndex + 3);
+
+                        var currentRow = 6;
+                        
+                        // Fill the rows
+                        foreach (var row in sheet.Rows)
+                        {
+                            worksheet.Cell(currentRow, 1).Value = row.Date;
+                            worksheet.Cell(currentRow, 5).Value = row.TotalMiles;
+
+                            foreach (var description in row.Description)
+                            {
+                                worksheet.Cell(currentRow, 2).Value = description;
+                                currentRow++;
+                            }
+                        }
                     }
+
+                    // Save workbook to memory
+                    using var bookStream = new MemoryStream();
+                    workbook.SaveAs(bookStream);
+                    bookStream.Position = 0;
+
+                    // Add XLSX to ZIP
+                    var entryName =
+                        $"Mileage_Report_{bookIndex + 1}_{startDate:yyyy-MM-dd}_to_{endDate:yyyy-MM-dd}.xlsx";
+
+                    // Add the file to the archive
+                    var entry = archive.CreateEntry(entryName, CompressionLevel.Fastest);
+
+                    await using var entryStream = await entry.OpenAsync();
+                    await bookStream.CopyToAsync(entryStream);
                 }
             }
-            
-            using var stream = new MemoryStream();
-            
-            workbook.SaveAs(stream);
-            stream.Position = 0;
-            
-            return File(stream.ToArray(), 
-                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                $"Mileage_Report_{startDate:yyyy-MM-dd}_to_{endDate:yyyy-MM-dd}.xlsx");
+
+            zipStream.Position = 0;
+                
+            // Return a zip file
+            return File(
+                zipStream.ToArray(),
+                "application/zip",
+                $"Mileage_Reports_{startDate:yyyy-MM-dd}_to_{endDate:yyyy-MM-dd}.zip"
+            );
         }
 
-        private async Task<Book> CreateBookAsync(DateOnly startDate, DateOnly endDate)
+        private async Task<List<Book>> CreateBooksAsync(DateOnly startDate, DateOnly endDate)
         {
             // Get data from server
             var journeys = await _context.Journeys
-                .Where(j => j.Date > startDate && j.Date < endDate)
+                .Where(j => j.Date >= startDate && j.Date <= endDate)
                 .Include(j => j.HomeLocation)
                 .Include(j => j.Trips)
                 .ThenInclude(t => t.Location)
                 .Include(j => j.Trips)
                 .ThenInclude(t => t.Reason)
+                .OrderBy(j => j.Date)
                 .ToListAsync();
             
-            // Calculate mileage claim rate
-            var claimRate = await CalculateClaimRateAsync();
+            var books = new List<Book>();
             
-            // Build sheets
-            var sheets = BuildSheets(journeys);
+            // Create a list of journeys in a buffer
+            var bufferedJourneys = new List<Journey>();
+  
+            // Variable to hold previous state
+            bool? previousOverThreshold = null;
             
-            return new Book()
+            // Iterate over each journey
+            foreach (var journey in journeys)
             {
-                Sheets = sheets,
-                ClaimRate = claimRate
-            };
+                // Check if the current journey is over the threshold
+                var currentOverThreshold = await CalculateOverThresholdAsync(journey.Date);
+                
+                // First journey loop
+                if (previousOverThreshold == null)
+                {
+                    previousOverThreshold = currentOverThreshold;
+                
+                // The claim rate has changed, so previous book msut be finished
+                } else if (previousOverThreshold != currentOverThreshold)
+                {
+                    // Create the book at curent claim rate with buffered journeys
+                    books.Add(new Book()
+                    {
+                        IsOverThreshold = previousOverThreshold.Value,
+                        Sheets = BuildSheets(bufferedJourneys),
+                    });
+                    
+                    // Clear the buffered journeys
+                    bufferedJourneys.Clear();
+                    
+                    // Update state
+                    previousOverThreshold = currentOverThreshold;
+                }
+                
+                // Add to the buffered journeys
+                bufferedJourneys.Add(journey);
+            }
+            
+            // If any journeys are left, add to book
+            if (bufferedJourneys.Count != 0)
+            {
+                books.Add(new Book
+                {
+                    IsOverThreshold = previousOverThreshold!.Value,
+                    Sheets = BuildSheets(bufferedJourneys)
+                });
+            }
+            
+            return books;
         }
 
-        private async Task<double> CalculateClaimRateAsync()
+        private async Task<bool> CalculateOverThresholdAsync(DateOnly claimDate)
         {
-            var currentTaxYear = DateTime.Now.Month >= 4 ? DateTime.Now.Year : DateTime.Now.Year - 1;
-            var taxYearStart = new DateOnly(currentTaxYear, 4, 4);
+            var currentTaxYear = claimDate.Month >= 4 ? claimDate.Year : claimDate.Year - 1;
+            var taxYearStart = new DateOnly(currentTaxYear, 4, 6);
 
             var totalMilesSinceTaxYearStart = await _context.Journeys
-                .Where(j => j.Date >= taxYearStart)
+                .Where(j => j.Date >= taxYearStart && j.Date <= claimDate)
                 .SumAsync(j => j.TotalMiles);
 
-            return totalMilesSinceTaxYearStart > 10000 ? 0.25 : 0.45;
+            return totalMilesSinceTaxYearStart > 10000;
         }
 
         private List<Sheet> BuildSheets(List<Journey> journeys)
@@ -200,6 +272,6 @@ namespace WimpeyTrack.Api.Controllers
     public class Book
     {
         public ICollection<Sheet> Sheets { get; set; } = new List<Sheet>();
-        public double ClaimRate { get; set; }
+        public bool IsOverThreshold { get; set; }
     }
 }
